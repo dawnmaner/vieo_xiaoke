bcd8956204bc514498ad52d255b5bb7561af0a54
dawn1 <dawn1@liout.com>
2019-09-10 18:05:38 +0800

    组件结构和页面渲染,组件点击切换完成

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="main">
+        <Header/>
+        <Tabber/>
+        hello cinema
+    </div>
+</template>
+
+<script>
+    import Header from "@/components/Header";
+    import Tabber from '@/components/Tabber';
+    export default {
+        name: "Cinema",
+        components: {
+            Header,
+            Tabber,
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="main">
+        <Header title="我的电影"/>
+        <Tabber/>
+        hello mine
+    </div>
+</template>
+
+<script>
+    import Header from "@/components/Header";
+    import Tabber from '@/components/Tabber';
+    export default {
+        name: "Mine",
+        components: {
+            Header,
+            Tabber,
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
