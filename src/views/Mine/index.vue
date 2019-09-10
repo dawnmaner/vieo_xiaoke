da92ad7a5114fc5631d10e0672f1a72b09353d91
dawn1 <dawn1@liout.com>
2019-09-10 19:03:50 +0800

    movie,电影,影院,登录搭建完成

--- src/views/Mine/index.vue
+++ src/views/Mine/index.vue
@@ -1,19 +1,24 @@
 <template>
     <div id="main">
         <Header title="我的电影"/>
+
+            <Login/>
+
         <Tabber/>
-        hello mine
+
     </div>
 </template>
 
 <script>
     import Header from "@/components/Header";
     import Tabber from '@/components/Tabber';
+    import Login from '@/components/Login'
     export default {
         name: "Mine",
         components: {
             Header,
             Tabber,
+            Login
         },
     }
 </script>
