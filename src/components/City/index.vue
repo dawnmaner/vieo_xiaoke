257f6c59f0a192624d6e244c62093815433b4021
dawn1 <dawn1@liout.com>
2019-09-28 18:36:05 +0800

    城市组件数据渲染完成

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -1,83 +1,107 @@
 <template>
     <div class="city_body">
+        <!--<div class="city_list">-->
+            <!--<div class="city_hot">-->
+                <!--<h2>热门城市</h2>-->
+                <!--<ul class="clearfix">-->
+                    <!--<li>上海</li>-->
+                    <!--<li>北京</li>-->
+                    <!--<li>上海</li>-->
+                    <!--<li>北京</li>-->
+                    <!--<li>上海</li>-->
+                    <!--<li>北京</li>-->
+                    <!--<li>上海</li>-->
+                    <!--<li>北京</li>-->
+                <!--</ul>-->
+            <!--</div>-->
+            <!--<div class="city_sort">-->
+                <!--<div>-->
+                    <!--<h2>A</h2>-->
+                    <!--<ul>-->
+                        <!--<li>阿拉善盟</li>-->
+                        <!--<li>鞍山</li>-->
+                        <!--<li>安庆</li>-->
+                        <!--<li>安阳</li>-->
+                    <!--</ul>-->
+                <!--</div>-->
+                <!--<div>-->
+                    <!--<h2>B</h2>-->
+                    <!--<ul>-->
+                        <!--<li>北京</li>-->
+                        <!--<li>保定</li>-->
+                        <!--<li>蚌埠</li>-->
+                        <!--<li>包头</li>-->
+                    <!--</ul>-->
+                <!--</div>-->
+                <!--<div>-->
+                    <!--<h2>A</h2>-->
+                    <!--<ul>-->
+                        <!--<li>阿拉善盟</li>-->
+                        <!--<li>鞍山</li>-->
+                        <!--<li>安庆</li>-->
+                        <!--<li>安阳</li>-->
+                    <!--</ul>-->
+                <!--</div>-->
+                <!--<div>-->
+                    <!--<h2>B</h2>-->
+                    <!--<ul>-->
+                        <!--<li>北京</li>-->
+                        <!--<li>保定</li>-->
+                        <!--<li>蚌埠</li>-->
+                        <!--<li>包头</li>-->
+                    <!--</ul>-->
+                <!--</div>-->
+                <!--<div>-->
+                    <!--<h2>A</h2>-->
+                    <!--<ul>-->
+                        <!--<li>阿拉善盟</li>-->
+                        <!--<li>鞍山</li>-->
+                        <!--<li>安庆</li>-->
+                        <!--<li>安阳</li>-->
+                    <!--</ul>-->
+                <!--</div>-->
+                <!--<div>-->
+                    <!--<h2>B</h2>-->
+                    <!--<ul>-->
+                        <!--<li>北京</li>-->
+                        <!--<li>保定</li>-->
+                        <!--<li>蚌埠</li>-->
+                        <!--<li>包头</li>-->
+                    <!--</ul>-->
+                <!--</div>-->
+            <!--</div>-->
+        <!--</div>-->
+        <!--<div class="city_index">-->
+            <!--<ul>-->
+                <!--<li>A</li>-->
+                <!--<li>B</li>-->
+                <!--<li>C</li>-->
+                <!--<li>D</li>-->
+                <!--<li>E</li>-->
+            <!--</ul>-->
+        <!--</div>-->
         <div class="city_list">
             <div class="city_hot">
                 <h2>热门城市</h2>
                 <ul class="clearfix">
-                    <li>上海</li>
-                    <li>北京</li>
-                    <li>上海</li>
-                    <li>北京</li>
-                    <li>上海</li>
-                    <li>北京</li>
-                    <li>上海</li>
-                    <li>北京</li>
+                    <li v-for="item in hotList" :key="item.id">{{item.nm}}</li>
                 </ul>
             </div>
-            <div class="city_sort">
-                <div>
-                    <h2>A</h2>
+            <div class="city_sort" ref="city_sort">
+                <div v-for="item in cityList" :key="item.index">
+                    <h2>{{item.index}}</h2>
                     <ul>
-                        <li>阿拉善盟</li>
-                        <li>鞍山</li>
-                        <li>安庆</li>
-                        <li>安阳</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>A</h2>
-                    <ul>
-                        <li>阿拉善盟</li>
-                        <li>鞍山</li>
-                        <li>安庆</li>
-                        <li>安阳</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>A</h2>
-                    <ul>
-                        <li>阿拉善盟</li>
-                        <li>鞍山</li>
-                        <li>安庆</li>
-                        <li>安阳</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
+                        <li v-for="itemList in item.list" :key="itemList.id">
+                            {{itemList.nm}}
+                        </li>
                     </ul>
                 </div>
             </div>
         </div>
         <div class="city_index">
             <ul>
-                <li>A</li>
-                <li>B</li>
-                <li>C</li>
-                <li>D</li>
-                <li>E</li>
+                <li v-for="(item,index) in cityList" :key="item.index"
+                @touchstart="handleToIndex(index)">{{item.index}}</li>
             </ul>
         </div>
     </div>
@@ -85,13 +109,90 @@
 
 <script>
     export default {
-        name: "City"
+        name: "City",
+        data(){
+           return {
+               cityList: [],
+               hotList: [],
+           }
+        },
+        mounted() {
+            this.axios.get('/api/cityList').then((res)=>{
+                console.log(res);
+                var msg=res.data.msg;
+                if(msg === 'ok'){
+                var cities =res.data.data.cities;
+                //[index:'A',list:[{nm:'阿城',id:123}]
+                var {cityList,hotList}=this.formatCityList(cities);
+                this.cityList=cityList;
+                this.hotList=hotList;
+                }
+            }),
+            console.log(this);
+
+        },
+        methods:{
+            formatCityList(cities){
+                var cityList=[];//城市的分类
+                var hotList=[];//热门城市
+
+                for(var i=0;i<cities.length;i++){
+                    if(cities[i].isHot ===1){
+                        hotList.push(cities[i]);
+                    }
+                }
+
+
+                for(var i=0;i<cities.length;i++) {
+                        //cities下每一个py
+                    var firstLetter=cities[i].py.substring(0,1).toUpperCase();
+                    //判断是否存在firstLetter结构体
+                    if (toCom(firstLetter)){ //新添加index
+                        cityList.push({index:firstLetter,list:[{nm:cities[i].nm,id:cities[i].id}]})
+                    }else {//累加到已有index
+                        for (var j=0;j<cityList.length;j++){
+                            if(cityList[j].index ===firstLetter){
+                                cityList[j].list.push({nm:cities[i].nm,id:
+                                cities[i].id});
+                            }
+                        }
+                    }
+                }
+                cityList.sort((n1,n2)=>{
+                    if(n1.index >n2.index){
+                        return 1;
+                    }else if(n1.index <n2.index){
+                        return -1;
+                    }else{
+                        return 0;
+                    }
+                })
+                function toCom(firstLetter) {
+                    for(var i=0;i<cityList.length;i++){
+                        if(cityList[i].index ===firstLetter){
+                            return false;//如果比对成功就添加
+                        }
+                    }
+                    return true;
+                    
+                }
+               // console.log(hotList)
+                return {
+                    cityList,hotList
+                }
+            },
+            handleToIndex(index){
+                    var h2=this.$refs.city_sort.getElementsByTagName('h2');
+                    //parentNode是外层的city_list
+                   this.$refs.city_sort.parentNode.scrollTop= h2[index].offsetTop;
+            }
+        }
     }
 </script>
 
 <style scoped>
 
-    .city_body { margin-top:97px; display: flex; width: 100%; position: absolute; top: 0; bottom: 0;}
+    .city_body { margin-top: 97px; display: flex; width: 100%; position: absolute; top: 0; bottom: 0;}
 
     .city_body .city_list { flex: 1; overflow: auto; background: #FFF5F0;}
 
